a43f3f957bd82342ce43f51ee0b992a101826de2
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2024-10-30 20:19:23 -0300

    chore: changed build script

--- back/build.sh
+++ back/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+yarn build:webpack
+
 echo "require(\"./main\");" > dist/index.js
+
 echo "# .gitignore
 node_modules/
 package-lock.json" > dist/.gitignore
+
 cp dist.json dist/package.json
