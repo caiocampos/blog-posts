e7aa49e162f2a1b59051e3c5d2d78776647c8f91
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2024-10-30 20:12:53 -0300

    chore: build updated

--- back/build.sh
+++ back/build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-yarn build:webpack
-echo "{ pkgs }: {
-	deps = [
-		pkgs.nodejs-16_x
-	];
-}" > dist/replit.nix
 echo "require(\"./main\");" > dist/index.js
-echo "run = \"npm install && npm start\"" > dist/.replit
 echo "# .gitignore
 node_modules/
 package-lock.json" > dist/.gitignore
