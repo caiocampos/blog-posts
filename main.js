339d7d6782c95787dafe2349f9fadbeca1f9980a
caiocampos <20524962+caiocampos@users.noreply.github.com>
2024-11-06 01:38:19 +0000

    Deploying to dist from @ caiocampos/blog-posts@716f0e1bd2a8aefa24f0002d6afd2e38671650a1 🚀

--- main.js
+++ main.js
@@ -724,7 +724,7 @@ exports.PostsService = PostsService = PostsService_1 = __decorate([
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
 (() => {
 var exports = __webpack_exports__;
 
