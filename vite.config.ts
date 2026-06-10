6d69f9192d28f04dd38269b96cc15bc60b2e3f18
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2026-06-10 12:49:18 -0300

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { dependencies } from './package.json';
-
-const vendorReactList = ['react', 'react-dom', 'react-router-dom'];
-const vendorAntdList = ['antd', '@ant-design/pro-layout', '@ant-design/pro-table'];
-
-function renderChunks(deps: Record<string, string>) {
-	const chunks: string[] = [];
-	Object.keys(deps).forEach((key) => {
-		if (vendorReactList.includes(key) || vendorAntdList.includes(key)) {
-			return;
-		}
-		chunks.push(key);
-	});
-	return chunks;
-}
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -31,10 +16,33 @@ export default defineConfig({
 		chunkSizeWarningLimit: 1000,
 		rollupOptions: {
 			output: {
-				manualChunks: {
-					vendor_react: vendorReactList,
-					vendor_antd: vendorAntdList,
-					vendor: renderChunks(dependencies)
+				codeSplitting: {
+					groups: [
+						{
+							test: /node_modules\/react/,
+							name: 'react'
+						},
+						{
+							test: /node_modules\/react-dom/,
+							name: 'react-dom'
+						},
+						{
+							test: /node_modules\/react-router-dom/,
+							name: 'react-router-dom'
+						},
+						{
+							test: /node_modules\/antd/,
+							name: 'antd'
+						},
+						{
+							test: /node_modules\/@ant-design\/pro-layout/,
+							name: 'antd-pro-layout'
+						},
+						{
+							test: /node_modules\/@ant-design\/pro-table/,
+							name: 'antd-pro-table'
+						}
+					]
 				}
 			}
 		}
