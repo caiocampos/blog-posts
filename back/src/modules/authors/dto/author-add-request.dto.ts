3089bf5c7312827656ef4fc13a22547f3c5a792c
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2026-07-06 19:31:10 -0300

    change validation

--- back/src/modules/authors/dto/author-add-request.dto.ts
+++ back/src/modules/authors/dto/author-add-request.dto.ts
@@ -1,15 +1,18 @@
-import { IsNotEmpty, IsDateString } from 'class-validator';
+import { IsNotEmpty, IsDateString, IsString } from 'class-validator';
 import { ValidationMessages } from '../../../common/messages/validation-messages.constants';
 
 export default class AuthorAddRequestDTO {
+  @IsString()
   @IsNotEmpty({ message: ValidationMessages.IS_NOT_EMPTY })
   name: string;
 
+  @IsString()
   @IsNotEmpty({ message: ValidationMessages.IS_NOT_EMPTY })
   nickname: string;
 
+  @IsString()
   @IsNotEmpty({ message: ValidationMessages.IS_NOT_EMPTY })
-  @IsDateString({}, { message: ValidationMessages.IS_NOT_EMPTY })
+  @IsDateString(undefined, { message: ValidationMessages.IS_NOT_EMPTY })
   birthDate: string;
 
   public constructor(name: string, nickname: string, birthDate: string) {
--- back/src/modules/posts/dto/post-add-request.dto.ts
+++ back/src/modules/posts/dto/post-add-request.dto.ts
@@ -1,10 +1,12 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { ValidationMessages } from '../../../common/messages/validation-messages.constants';
 
 export default class PostAddRequestDTO {
+  @IsString()
   @IsNotEmpty({ message: ValidationMessages.IS_NOT_EMPTY })
   title: string;
 
+  @IsString()
   @IsNotEmpty({ message: ValidationMessages.IS_NOT_EMPTY })
   body: string;
 
